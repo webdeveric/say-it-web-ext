53712bb6acafe0a4d5b5fba2512caf5cf00df955
Eric King <eric@webdeveric.com>
2023-10-14 09:21:17 -0500

    chore: updated build

--- web-ext-config.js
+++ web-ext-config.js
@@ -1,16 +1,25 @@
-const path = require('path');
+import { join } from 'node:path';
+import { fileURLToPath } from 'node:url';
 
-const webExtId = require('./src/manifest.json').browser_specific_settings.gecko.id;
+import manifest from './src/manifest.json' assert { type: 'json' };
 
-module.exports = {
-  verbose: true,
-  sourceDir: path.join(__dirname, 'dist'),
-  artifactsDir: path.join(__dirname, 'build'),
-  build: {
-    overwriteDest: true,
-  },
-  run: {
-    startUrl: [`about:devtools-toolbox?id=${encodeURIComponent(webExtId)}&type=extension`, 'https://webdeveric.com/'],
-  },
-  ignoreFiles: ['package-lock.json'],
+const webExtId = manifest.browser_specific_settings.gecko.id;
+
+// eslint-disable-next-line @typescript-eslint/naming-convention
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
+
+export const verbose = true;
+
+export const sourceDir = join(__dirname, 'dist');
+
+export const artifactsDir = join(__dirname, 'build');
+
+export const build = {
+  overwriteDest: true,
 };
+
+export const run = {
+  startUrl: [`about:devtools-toolbox?id=${encodeURIComponent(webExtId)}&type=extension`, 'https://webdeveric.com/'],
+};
+
+export const ignoreFiles = ['package-lock.json'];
